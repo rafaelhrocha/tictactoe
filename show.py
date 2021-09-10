5e5b81f8b79b403bc241bfb6c164335a66e5d31e
rafaelhrocha <rafaelehnrq@gmail.com>
2021-09-10 14:03:51 -0300

    first commit

---
+++ show.py
@@ -0,0 +1,10 @@
+from ttt import *
+
+if __name__ == "show":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
