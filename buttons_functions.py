f4e272f677ad30384e61d608551a8e89328ff5c7
rafaelhrocha <rafaelehnrq@gmail.com>
2021-09-10 14:49:38 -0300

    first commit

---
+++ buttons_functions.py
@@ -0,0 +1,9 @@
+from ttt import Ui_MainWindow
+
+class Buttons_controler(Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+    
+
+    def set_position(self, player):
+        pass
